9f8c81bf51250ad7d5fb9c9da0722ce704987566
Kelly Lima <keely06@live.com>
2019-08-04 20:45:51 -0300

    add date/time

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,24 +1,58 @@
 class CalcController {
     constructor() {
+        //forma de convensão de dizer que é um atributo privado, apenas a classe tem acesso
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
 
         //this faz referencia ao objeto que estou instanciando lá em calculadora
-        this._displayCalc = "0"; //forma de convensão de dizer que é um atributo privado, apenas a classe tem acesso
-        this._dataAtual; 
+        this.currentDate;
+        this.initialize();
+    }
+
+    initialize() {
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+            this.setDisplayDateTime();
+        }, 1000);
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
     }
 
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor) {
-        this._displayCalc = valor; //set não tem return, atribuição do valor
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value; //set não tem return, atribuição do valor
     }
 
-    get dataAtual() {
-        return this._dataAtual;
+    get currentDate() {
+        return new Date();
     }
 
-    set dataAtual(data) {
-        this._dataAtual = data;
+    set currentDate(date) {
+        this.currentDate = date;
     }
 }
